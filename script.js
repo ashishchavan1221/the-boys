b88fdf80049d19aea962e0d9a99bcbcf5beea6ff
kumar <krishnaraogandupalli2000@gmail.com>
2025-09-24 16:48:01 +0530

    Update Files

---
+++ script.js
@@ -0,0 +1,9 @@
+let Head1 = document.getElementById("Head1");
+let Head2 = document.getElementById("Head2");
+let Para = document.getElementById("Para");
+let Para2 = document.getElementById("Para2");
+
+Head1.classList.add("heading");
+Head2.classList.add("heading");
+Para.classList.add("para");
+Para2.classList.add("para");
